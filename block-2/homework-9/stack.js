5608dabb9d8ba31838594e0138b5cd5d7332ddd2
Lucas Montecino <lucasmontecino95@gmail.com>
2025-08-04 16:45:36 -0300

    feat: finish part 2 exercise 1

--- block-2/homework-9/stack.js
+++ block-2/homework-9/stack.js
@@ -15,3 +15,39 @@ export class Stack {
     return this.stack.at(-1);
   }
 }
+
+export class MinMaxStack extends Stack {
+  constructor() {
+    super();
+    this.minStack = [];
+    this.maxStack = [];
+  }
+
+  push(item) {
+    this.stack.push(item);
+
+    const min =
+      this.minStack.length === 0 ? item : Math.min(item, this.minStack.at(-1));
+
+    this.minStack.push(min);
+
+    const max =
+      this.maxStack.length === 0 ? item : Math.max(item, this.maxStack.at(-1));
+
+    this.maxStack.push(max);
+  }
+
+  pop() {
+    this.minStack.pop();
+    this.maxStack.pop();
+    return this.stack.pop();
+  }
+
+  getMin() {
+    return this.minStack.at(-1);
+  }
+
+  getMax() {
+    return this.maxStack.at(-1);
+  }
+}
